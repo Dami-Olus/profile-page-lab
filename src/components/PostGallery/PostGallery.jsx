d9c00b0a77fe03ff4c79b66fa0616d07656f9faf
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-27 15:34:17 -0400

    first commit

---
+++ src/components/PostGallery/PostGallery.jsx
@@ -0,0 +1,17 @@
+
+import {Card} from 'semantic-ui-react'
+import PostCard from '../PostCard/PostCard'
+
+
+export default function PostGallery({posts}){
+
+    const postCards = posts.map((post) => {
+        return <PostCard post={post} key={post._id}/> 
+    })
+
+    return (
+       <Card.Group itemsPerRow={1}>
+        {postCards}
+       </Card.Group>
+    )
+}
