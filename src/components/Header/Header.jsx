c4ebb22841861b3b8214b231d1a36caf105eccca
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-29 14:31:21 -0400

    home icon linked to homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Button, Header, Icon, Image, Segment } from "semantic-ui-react";
 import { Input, Menu } from "semantic-ui-react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function PageHeader({ user }) {
   return (
@@ -8,10 +8,14 @@ export default function PageHeader({ user }) {
       <Header as={Link} to={`/${user.username}`} floated="left">
         <Image circular src={user.photoUrl} />
       </Header>
-      <Header as="h2" floated="right">
-        <Icon name="home" color="blue" />
+      <Header floated="right">
+        <Header  floated="left" as={Link} to={`/`}>
+          <Icon name="home" color="blue" />
+        </Header>
+        <Header as="h2" floated="right" color="blue">
+          Logout
+        </Header>
       </Header>
     </>
-    
   );
 }
